00d1352755010bc59af6b5c9397df4e2e111efba
nivapaul890 <nivapaul890@gmail.com>
2024-01-11 08:33:55 -0800

    This is abstraction program

---
+++ abstraction.py
@@ -0,0 +1,20 @@
+from abc import ABC,abstractmethod
+#abstractclass
+class shape(ABC):
+    @abstractmethod
+    def area(self):
+        print('calculating the area')
+class Circle(shape):
+    def __init__(self,r):
+        self.r=r
+    def area(self):
+        print('area of circle is',3.14*self.r**2)
+class Square(shape):
+    def __init__(self,s):
+        self.s=s
+    def area(self):
+        print('area of square is  ',self.s**2)
+c=Circle(4)
+c.area()
+s=Square(6)
+s.area()
