770e232d1cbc06171561a11f4334be33c04b733f
nivapaul890 <nivapaul890@gmail.com>
2024-01-11 08:17:32 -0800

    This is a sample python program

---
+++ sample.py
@@ -0,0 +1,10 @@
+import rectangle
+#s=rectangle.rect_a(3,4)
+#print('Area of a rectangle is:',s)
+#print('area of a rectangle is:',rectangle.rect_a(5,6))
+l=12;w=3
+r=rectangle.rect_a(l,w)
+print('area of rectangle is:',r)
+s=5
+sq=rectangle.square_a(s)
+print('area of square is:',sq)
